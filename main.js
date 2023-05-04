a490864e8ade4480fbe3a71c780252556341166c
VINICIUS LOPES ALVES <61030197+vanilanorca@users.noreply.github.com>
2023-05-04 13:19:47 -0300

    alterações transito

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 var waze = './waze.json'
 
+// Cores transito
+var verde = "#1AB277"
+var amarelo = "#FFDC5A"
+var laranja = "#F5A762"
+var vermelho = "#FE546E"
+
+
+
 fetch(waze)
   .then(response => response.json())
   .then(data => {
     
     // VOLTA
-    var nomeVolta = document.getElementById('nomeVolta')
-    var voltaPartida = document.getElementById('voltaPartida')
-    var voltaChegada = document.getElementById('voltaChegada')
-    var voltaTempo = document.getElementById('voltaTempo')
-    var voltaDistancia = document.getElementById('voltaDistancia')
+    const nomeVolta = document.getElementById('nomeVolta')
+    const voltaPartida = document.getElementById('voltaPartida')
+    const voltaChegada = document.getElementById('voltaChegada')
+    const voltaTempo = document.getElementById('voltaTempo')
+    const voltaDistancia = document.getElementById('voltaDistancia')
+    const transitoVoltaTexto = document.getElementById('transitoVoltaTexto')
+    const transitoVoltaCor = document.getElementById('transitoVoltaCor')
 
     let rotaVolta = data.routes[0]
+
     nomeVolta.innerHTML = rotaVolta.name
     voltaPartida.innerHTML = rotaVolta.fromName
     voltaChegada.innerHTML = rotaVolta.toName
@@ -23,7 +34,6 @@ fetch(waze)
         let seg = segundos % 60;
 
         voltaTempo.innerHTML = (horas + "h " + minutos + "min " + seg + "s")
-
     }
     ajusteTempoVolta()
 
@@ -33,13 +43,33 @@ fetch(waze)
     }
     ajusteDistanciaVolta()
 
+    function transitoVolta(){
+        var transitoVolta = rotaVolta.jamLevel
+        if(transitoVolta == 0 || transitoVolta == 1){
+            transitoVoltaTexto.innerHTML = "Livre"
+            transitoVoltaCor.style.background = verde
+        }else if(transitoVolta == 2){
+            transitoVoltaTexto.innerHTML = "Moderado"
+            transitoVoltaCor.style.background = amarelo
+        }else if(transitoVolta == 3){
+            transitoVoltaTexto.innerHTML = "Intenso"
+            transitoVoltaCor.style.background = laranja
+        }else if(transitoVolta == 4){
+            transitoVoltaTexto.innerHTML = "Paralizado"
+            transitoVoltaCor.style.background = vermelho
+        }
+    }
+    transitoVolta()
+
 
     // IDA
-    var nomeIda = document.getElementById('nomeIda')
-    var idaPartida = document.getElementById('idaPartida')
-    var idaChegada = document.getElementById('idaChegada')
-    var idaTempo = document.getElementById('idaTempo')
-    var idaDistancia = document.getElementById('idaDistancia')
+    const nomeIda = document.getElementById('nomeIda')
+    const idaPartida = document.getElementById('idaPartida')
+    const idaChegada = document.getElementById('idaChegada')
+    const idaTempo = document.getElementById('idaTempo')
+    const idaDistancia = document.getElementById('idaDistancia')
+    const transitoIdaTexto = document.getElementById('transitoIdaTexto')
+    const transitoIdaCor = document.getElementById('transitoIdaCor')
 
     let rotaIda = data.routes[1]
     nomeIda.innerHTML = rotaIda.name
@@ -53,7 +83,6 @@ fetch(waze)
         let seg = segundos % 60;
 
         idaTempo.innerHTML = (horas + "h " + minutos + "min " + seg + "s")
-
     }
     ajusteTempoIda()
 
@@ -62,6 +91,24 @@ fetch(waze)
         idaDistancia.innerHTML = rotaIda.length + " km"
     }
     ajusteDistanciaIda()
+
+    function transitoIda(){
+        var transitoIda = rotaIda.jamLevel
+        if(transitoIda == 0 || transitoIda == 1){
+            transitoIdaTexto.innerHTML = "Livre"
+            transitoIdaCor.style.background = verde
+        }else if(transitoIda == 2){
+            transitoIdaTexto.innerHTML = "Moderado"
+            transitoIdaCor.style.background = amarelo
+        }else if(transitoIda == 3){
+            transitoIdaTexto.innerHTML = "Intenso"
+            transitoIdaCor.style.background = laranja
+        }else if(transitoIda == 4){
+            transitoIdaTexto.innerHTML = "Paralizado"
+            transitoIdaCor.style.background = vermelho
+        }
+    }
+    transitoIda()
 
     console.log(rotaIda)
     console.log(rotaVolta)
@@ -77,22 +124,3 @@ fetch(waze)
 
 
 
-
-
-
-
-
-// let lastJson = null;
-
-// function checkForChanges() {
-//     fetch("https://www.waze.com/row-partnerhub-api/feeds-tvt/?id=11497612172")
-//       .then(response => response.json())
-//       .then(json => {
-//         if (JSON.stringify(json) !== JSON.stringify(lastJson)) {
-//           console.log("JSON mudou:", json);
-//           lastJson = json;
-//         }
-//       })
-//       .catch(error => console.error(error));
-//   }
-//   setInterval(checkForChanges, 5000);
